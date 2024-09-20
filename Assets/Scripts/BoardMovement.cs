8fc3d4039bbd656621e2730f0aa8d609619ac704
Earthan Huq <EarthanTaan@users.noreply.github.com>
2024-09-19 20:23:05 -0400

    Adding sound
    I think I sort of get it?
    You have to declare the sound outside,
    Then assign the sound inside.
    The assigner seems inefficient, but what do I know? (Apparently you can only assign an AudioSource by telling it to get the AudioSource Component that's attached to the self-GameObject)

--- Assets/Scripts/BoardMovement.cs
+++ Assets/Scripts/BoardMovement.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class BoardMovement : MonoBehaviour
 {
-    public Transform myHazard;
+    //public Transform myHazard; (rendered obsolete by the following array)
+    public Transform[] myHazards;
+
     //public int myInt = 5;
     Vector3 startPos;
+
+    //Declare a new variable of datatype 'AudioSource' with a name:
+    AudioSource dortSound;
 
     void Start()
     {
         startPos = transform.position;
+        //Assign variable the value taken from the AudioSource Component attached to 'this' GameObject:
+        dortSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -33,10 +40,21 @@ public class BoardMovement : MonoBehaviour
             transform.position += Vector3.back;
         }
 
-        if (transform.position == myHazard.position)
+        //if (transform.position == myHazard.position)
+        //{
+        //    Debug.Log("BOOM");
+        //    transform.position = startPos;
+        //}
+
+        for (int howManyTimesLoopHasRun = 0; howManyTimesLoopHasRun < myHazards.Length; howManyTimesLoopHasRun++)
         {
-            Debug.Log("BOOM");
-            transform.position = startPos;
+            if (transform.position == myHazards[howManyTimesLoopHasRun].position)
+            {
+                //Take the AudioSource referred to by the variable and execute its Play function:
+                dortSound.Play();
+                Debug.Log("BOOM!");
+                transform.position = startPos;
+            }
         }
     }
 }
