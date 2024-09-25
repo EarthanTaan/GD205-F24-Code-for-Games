e353c0dc9d25b1ae7444b6b96fa8f892cb61f77f
Earthan <EarthanTaan@users.noreply.github.com>
2024-09-25 16:15:46 -0400

    Man I don't know

--- Assets/Scripts/BoardMovement.cs
+++ Assets/Scripts/BoardMovement.cs
@@ -7,20 +7,25 @@ using UnityEngine;
 
 public class BoardMovement : MonoBehaviour
 {
+    // An array of Transforms called 'myHazards'. 
     public Transform[] myHazards;
+
+    // An array of Transforms called 'walkables'. In the editor this array has been populated with the floor cubes. (Incidentally, those cubes are also all tagged: 'Walkable',
+    // but I don't know how to utilize that.)
     public Transform[] walkables;
 
     // 'Vector3' is a variable that contains a 3D vector coordinate in the form of (x, y, z).
     Vector3 startPos;
 
-    // Declare a new variable of datatype 'AudioSource' with the name "dortSound":
+    // Declare a new variable of datatype 'AudioSource' with the name "dortSound"
     AudioSource dortSound;
 
     void Start()
     {
+        // Define the startPos as being the position the player is in when the game begins.
         startPos = transform.position;
 
-        //Assign variable the value taken from the AudioSource Component attached to 'this' GameObject:
+        //Assign variable the value taken from the AudioSource Component attached to the assigned GameObject. (Note: "this" refers to this script, not the gameObject)
         dortSound = GetComponent<AudioSource>();
     }
 
