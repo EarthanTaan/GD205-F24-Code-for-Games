467996ffb0add7fed0dc7840953dd9f9a0b4c12c
Earthan <EarthanTaan@users.noreply.github.com>
2024-10-31 12:17:42 -0400

    I broke one of my NPCs oops
    Oh well he's gone

--- Assets/Scripts/BoardMovement.cs
+++ Assets/Scripts/BoardMovement.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoardMovement : MonoBehaviour
