a3614712afb7ab3311a5cab372075a27d2a924e9
Earthan <EarthanTaan@users.noreply.github.com>
2024-11-14 20:40:17 -0500

    what are Quaternions?
    I have been blessed by the Quaternions

---
+++ Assets/Scripts/CarrierControls.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CarrierControls : MonoBehaviour
+{
+    //create a new Rigidbody called rb to manipulate in our script
+    Rigidbody rb;
+    Quaternion righted = new Quaternion();
+
+    //make a new public variable to change our acceleration force
+    public float acc = 3f;
+    public float speed = 50f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Physics.IgnoreCollision(rb.GetComponent<Collider>(), rb.GetComponent<Collider>(), true); //I don't think I need this
+
+        righted = rb.rotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //if (rb.rotation.z != 0f && !Input.anyKeyDown)
+        //{
+            rb.rotation = Quaternion.Slerp(rb.rotation, righted, 0.04f);
+        //}
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0f, 0f, acc, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0f, 0f, -acc, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-acc, 0f, 0f, ForceMode.Force);
+                if (rb.rotation.z < 30f)
+                {
+                    rb.AddTorque(0f, 0f, 0.5f, ForceMode.Force);    //for some reason, this line of code is also giving me the same behavior on the opposite side. If it ain't broke I won't fix it.
+                }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(acc, 0f, 0f, ForceMode.Force);
+            rb.AddTorque(0f, 0f, -0.5f);
+        }
+    }
+}
