dae916834f7e9c995ebb6d5c4afba01d85fe3487
Earthan <EarthanTaan@users.noreply.github.com>
2024-12-12 04:34:08 -0500

    Fixed ship tilt
    It took 4 hours, so now I won't be able to finish the actual project, because I got stuck on this one stupid detail.

--- Assets/Scripts/CarrierControls.cs
+++ Assets/Scripts/CarrierControls.cs
@@ -5,27 +5,30 @@ public class CarrierControls : MonoBehaviour
 {
     //create a new Rigidbody called rb to manipulate in our script
     Rigidbody rb;
-    Quaternion righted;
+    Vector3 righted;
 
     //make a new public variable to change our acceleration force (this becomes accessible from the editor side, in the component)
     public float thrust = 50f;
-    public float roll = 20f;
+    public float roll = 35f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
 
-        //righted = rb.rotation;        //abandoning this goal but leaving the scaffold, such as it is, in case I want to revisit this laeet
+        righted = transform.eulerAngles;        //grab the Vector3 for when the ship is at neutral rotation
     }
 
     void FixedUpdate()
     {
-        transform.Translate(0,0,Time.deltaTime);
+        transform.Translate(0,0,Time.deltaTime * 3f);       // "At rest", the ship moves forward constantly at a slow pace.
 
-        //if (rb.rotation.z != 0f && !Input.anyKeyDown)
-        //{
-        //    rb.rotation = Quaternion.Slerp(rb.rotation, righted, 0.04f);
-        //}
+        if (transform.eulerAngles.z != 0f && !Input.anyKeyDown)     //if the z-axis rotation is anything but neutral, and no key is being pressed:
+        {
+            Vector3 unRighted = transform.eulerAngles;
+            unRighted.z = Mathf.LerpAngle(unRighted.z, righted.z, Time.deltaTime * 2f);
+            transform.eulerAngles = unRighted;      //lerp back to 'righted'
+            
+        }
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -38,6 +41,9 @@ public class CarrierControls : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(-thrust, 0f, 0f, ForceMode.Acceleration);
+            Vector3 leftRoll = transform.eulerAngles;
+            leftRoll.z = Mathf.LerpAngle(leftRoll.z, roll, Time.deltaTime * 3f);
+            transform.eulerAngles = leftRoll;
             //if (transform.eulerAngles.z < 30f)
             //{
             //    transform.Rotate(transform.position.x, transform.position.y, transform.position.z + roll);        //I want to roll the ship to the left as it banks leftward, but I can't find he magic words.
@@ -47,6 +53,17 @@ public class CarrierControls : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(thrust, 0f, 0f, ForceMode.Acceleration);
+            Vector3 rightRoll = transform.eulerAngles;
+            rightRoll.z = Mathf.LerpAngle(rightRoll.z, -roll, Time.deltaTime * 3f);
+            transform.eulerAngles = rightRoll;
+            //  holy shit it works, it took four fucking hours but it works
+            /*  Griping:
+             *  It seems very clumsy and stupid to me that the only way I could manage this was by pulling apart the pieces of the Vector3, changing one bit, and then
+             *  putting it back. Why could't I just alter one component directly? I don't understand why I can alter an entire transform.eulerAngles at once,
+             *  but not transform.eulerAngles.z in isolation (unless I pull it out to a variable and then shove the whole variable back into the component. I must be missing
+             *  something, it's just too ridiculous.
+             */
+
         }
     }
 }
--- Assets/Scripts/bgScroll.cs
+++ Assets/Scripts/bgScroll.cs
@@ -23,8 +23,6 @@ public class bg_scroll : MonoBehaviour
     {
         Carrier = GameObject.Find("Carrier");
         CarrierRB = GameObject.Find("Carrier").GetComponent<Rigidbody>();
-        spaceRenderer = GetComponent<Renderer>();
-        
     }
 
     void Update()
@@ -33,9 +31,9 @@ public class bg_scroll : MonoBehaviour
         if (CarrierRB.linearVelocity.z > 0)
         {
             velMod = CarrierRB.linearVelocity.z * 0.01f;    //increase the rate of illusory speed by a fraction of the actual speed
-            if (velMod > 0.2f)
+            if (velMod > 0.02f)
             {
-                velMod = 0.2f;      //cap the speed
+                velMod = 0.02f;      //cap the speed
             }
         }
         else
@@ -47,7 +45,7 @@ public class bg_scroll : MonoBehaviour
         Material mat = mr.material;
         Vector2 offset = mat.mainTextureOffset;             //I guess we can't adjust the offset directly? We have to copy it to a variable, adust it and then send it back? I don't understand why, but that's what the tutorial I found said.
         offset.y += Time.deltaTime * (yMod+velMod);                  //I want the player to always be drifting forward a bit. This will scroll the material texture even while still.
-        offset.x = transform.position.x * xMod;             //slide the stars left and right in accordance with the ship's lateral motion.
+        offset.x = GameObject.Find("Carrier").transform.position.x * xMod;             //slide the stars left and right in accordance with the ship's lateral motion.
         mat.mainTextureOffset = offset;                     //send the adjustments back to the component in the editor.
     }
 }
